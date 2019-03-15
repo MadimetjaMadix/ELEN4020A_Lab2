6585e52617bab1ed7122b8d8a2a1a4dad00b0023
Lynch Mwaniki <lsking16@gmail.com>
2019-03-15 16:15:36 +0200

    Completed a block tranpose algorithm

--- pthread.c
+++ pthread.c
@@ -17,6 +17,7 @@ typedef struct{
 	int N;
 }ThreadDataDiagonal;
 
+
 void *diagonalAlg(void *arg)
 {
 	ThreadDataDiagonal* thread_data = (ThreadDataDiagonal*)arg;
@@ -51,7 +52,6 @@ void printTimeElasped(struct timeval start, struct timeval end)
 {
 	double time_taken = (end.tv_sec - start.tv_sec) * 1e6; 
     time_taken = (time_taken + (end.tv_usec -  start.tv_usec)) * 1e-6;
-	printf("\n------ Diagonal Algorithm ------\n");
 	printf("Total time taken by CPU: %f\n", time_taken);
 	printf("================================\n");
 
@@ -91,19 +91,161 @@ void runDiagonalAlgorithm(int* matrix, int N)
 	
 	//end timer
 	gettimeofday(&end, NULL);
+	printf("\n------ Diagonal Algorithm ------\n");
 	printTimeElasped(start, end);
+}
+
+//===============================BLOCK==========================================
+typedef struct
+{
+	int ID;
+	int row;
+	int column;
+	int N;
+	int* matrix;
+}ThreadDataBlock;
+
+int number_of_assigned_blocks       = 0;
+int next_block_column               = 0;
+int next_block_diagonal	            = 0;
+pthread_mutex_t updateBlockDiagonalLock;
+
+
+/*
+	Function implementing block transposition.
+*/
+void* blockAlgorithm(void* arg)
+{
+	ThreadDataBlock* thread_data = (ThreadDataBlock*)arg;
+	int N       = thread_data->N;
+	int N_block = N/2;
+	while(true)
+	{
+		int index[2] = {thread_data->row, thread_data->column};
+		int* blockA_indexes = blockElementsIndex2(index, N);
+		blockTranspose(thread_data->matrix, blockA_indexes);
+		if(thread_data->row != thread_data->column) // Not On diagonal:
+		{
+			swapElements(index, index+1);
+			int* blockB_indexes = blockElementsIndex2(index, N);
+			blockTranspose(thread_data->matrix, blockB_indexes);
+			
+			// Swap blocks:
+			swapBlocks(thread_data->matrix, blockA_indexes, blockB_indexes);
+			
+			free(blockA_indexes);
+			free(blockB_indexes);
+		}
+		
+		pthread_mutex_lock(&updateBlockDiagonalLock);
+		
+			thread_data->row    = next_block_diagonal;
+			thread_data->column = next_block_column;
+
+			++next_block_column;
+			if(next_block_column % N_block == 0 && next_block_column != 0)
+			{
+				++next_block_diagonal;
+				next_block_column = next_block_diagonal;	
+			}
+	
+			if(next_block_diagonal >= N_block && next_block_column >= N_block)
+			{
+				next_block_diagonal = N_block;
+				next_block_column = N_block;
+			}else number_of_assigned_blocks++;
+			
+		pthread_mutex_unlock(&updateBlockDiagonalLock);
+		
+		if(thread_data->row == N_block && thread_data->column == N_block) break;
+	}
+	
+	pthread_exit((void *) 0);
+}
+
+void runBlockTransposeAlgorithm(int* matrix, int N)
+{
+	struct timeval start, end;
+	int rc;
+	int N_block = N/2;
+	
+	// Create threads:
+	pthread_t threads[num_threads];
+	ThreadDataBlock threads_data[num_threads];
+	
+	for(int i = 0; i < (int)num_threads; ++i)
+	{
+	
+		int N_block = N/2;
+		++next_block_column;
+		if(next_block_column % N_block == 0 && next_block_column != 0)
+		{
+			++next_block_diagonal;
+			next_block_column = next_block_diagonal;	
+		}
+	
+		if(next_block_diagonal >= N_block && next_block_column >= N_block)
+		{
+			next_block_diagonal = N_block;
+			next_block_column = N_block;
+		}else number_of_assigned_blocks++;
+	}
+	
+	//start timer
+	gettimeofday(&start, NULL);
+	int row     = 0;
+	int column  = 0;
+	for(int i = 0; i < num_threads; ++i)
+	{
+		threads_data[i].ID = i;
+		if(column % N_block == 0 && column != 0)
+		{
+			++row;
+			column = row;	
+		}
+		
+		if(row >= N_block && column >= N_block)
+		{
+			row    = N_block;
+			column = N_block;
+		}
+		
+		threads_data[i].row     = row;
+		threads_data[i].column  = column++;
+		threads_data[i].matrix  = matrix;
+		threads_data[i].N       = N;
+		
+		rc = pthread_create(&threads[i], NULL, blockAlgorithm, &threads_data[i]);
+		
+		if(rc)
+		{
+			printf("ERROR creating thread");
+			exit(-1);
+		}
+	
+	}
+	
+	for(int j = 0; j < num_threads; ++j) pthread_join(threads[j], NULL);
+	
+	//end timer
+	gettimeofday(&end, NULL);
+	printf("\n------ Block Algorithm ------\n");
+	printTimeElasped(start, end);
+	
+	return;
 }
 
 int main()
 {
 	srand(time(NULL));
 	int N = 4096;
-	int *matrixA = createMarix(N);
-	
 	printf("No. of threads = %d \n", (int)num_threads);
 	
+	int *matrixA = createMarix(N);
+	
 	runDiagonalAlgorithm(matrixA, N);
-	
-	
+	runBlockTransposeAlgorithm(matrixA, N);
+
+	free(matrixA);
 	return 0;
 }
