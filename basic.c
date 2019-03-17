58e2ca9c397afb55b540cb97d27a841910584316
matsheket <41580351+matsheket@users.noreply.github.com>
2019-03-17 15:11:05 +0200

    Modified printing of results for basic algorithm

--- basic.c
+++ basic.c
@@ -4,9 +4,11 @@
 #include <time.h>
 #include "functions.h"
 
-
 void naiveAlgorithm(int *matrix, int N)
 {
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+
 	for(int i=0; i<N; ++i)
 	{
 		for(int j=i+1; j<N; ++j)
@@ -19,37 +21,43 @@ void naiveAlgorithm(int *matrix, int N)
 			swapElements(matrix+offsetA, matrix+offsetB);
 		}
 	}
-	
+	gettimeofday(&end, NULL);
+
+	double time_taken; 
+    time_taken = (end.tv_sec - start.tv_sec) * 1e6; 
+    time_taken = (time_taken + (end.tv_usec -  
+                              start.tv_usec)) * 1e-6;
+	printf("%10f ", time_taken);
+	return;
+}
+
+void runTransposeAlgorithm(int N)
+{
+	int *matrixA = createMarix(N);
+	naiveAlgorithm(matrixA, N);
+
+	free(matrixA);
+	return;
+}
+
+void printTableHeadings()
+{
+	printf("\n%5s %10d %12d %10d %10d \n%8s", "N | " , 128, 1024, 2048, 4096, " ");
+	return;
 }
 
 int main()
 {
-	struct timeval start, end;
+	srand(time(NULL));
+
+	printHeader();
+	printf("\nNumber of threads (Serial): %d", 1);
+	printTableHeadings();
 	
-	int N = 128;
-	srand(time(0));
-	
-	int *matrixA = createMarix(N);
-	
-	//start timer
-	gettimeofday(&start, NULL);
-	naiveAlgorithm(matrixA,N);
-	//end timer
-	gettimeofday(&end, NULL);
-
-
-	double time_taken; 
-  
-    time_taken = (end.tv_sec - start.tv_sec) * 1e6; 
-    time_taken = (time_taken + (end.tv_usec -  
-                              start.tv_usec)) * 1e-6;
-	printf("Total time taken by CPU: %f\n", time_taken  );
-
-	int x = 5;
-	int y = 8;
-	x = x + y++;
-	printf("%d",x);
-
+	runTransposeAlgorithm(128);
+	runTransposeAlgorithm(1024);
+	runTransposeAlgorithm(2048);
+	runTransposeAlgorithm(4096);
+	printf("\n");
 	return 0;
-
 }
